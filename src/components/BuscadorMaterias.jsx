b0b717c6c6f1e1cf6dcad8beca7605eddee6ecee
MatiasSolisSchneeberger <Matias.Solis.Sch@gmail.com>
2025-04-04 11:26:25 -0300

    cambiando diseño

--- src/components/BuscadorMaterias.jsx
+++ src/components/BuscadorMaterias.jsx
@@ -29,11 +29,11 @@ export default function BuscadorMaterias({ placeholder = "Buscar materia", targe
     };
 
     return (
-        <div className="w-full flex justify-center">
-            <div className="relative flex-grow">
-                {/* Lupa - SVG original */}
+
+        <div className="relative group flex flex-row rounded-md focus-within:ring-2 focus-within:ring-[var(--md-sys-color-tertiary)] transition-all duration-300">
+            {/* Contenedor de la lupa con z-index para superponer el ring */}
+            <div className='p-2.5 bg-[var(--md-sys-color-primary)] rounded-l-md text-[var(--md-sys-color-on-primary)] z-[1]'>
                 <svg
-                    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[var(--md-sys-color-outline)]"
                     xmlns="http://www.w3.org/2000/svg"
                     width={20}
                     height={20}
@@ -48,46 +48,43 @@ export default function BuscadorMaterias({ placeholder = "Buscar materia", targe
                     <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
                     <path d="M21 21l-6 -6" />
                 </svg>
-
-                <input
-                    type="text"
-                    placeholder={placeholder}
-                    value={searchTerm}
-                    onChange={handleSearch}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                    className={`w-full pl-10 pr-4 py-2 rounded-lg border border-[var(--md-sys-color-outline)] focus:outline-none focus:bg-[var(--md-sys-color-surface-variant)] focus:ring-2 focus:ring-[var(--md-sys-color-primary)] text-[var(--md-sys-color-on-surface)] focus:text-[var(--md-sys-color-on-surface-variant)] transition-all duration-300 ${(isFocused || searchTerm) ? 'max-w-full' : 'max-w-[220px]'}`}
-                />
-
-
-
-                {searchTerm && (
-                    <button
-                        onClick={() => {
-                            setSearchTerm('');
-                            handleSearch({ target: { value: '' } });
-                        }}
-                        className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[var(--md-sys-color-outline)]"
-                    >
-                        {/* X - SVG original */}
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M18 6l-12 12" />
-                            <path d="M6 6l12 12" />
-                        </svg>
-                    </button>
-                )}
             </div>
+
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={searchTerm}
+                onChange={handleSearch}
+                className='w-full px-4 py-2 rounded-r-md bg-[var(--md-sys-color-background)] border border-[var(--md-sys-color-outline)] focus:outline-none focus:bg-[var(--md-sys-color-surface-variant)] focus:ring-2 focus:ring-[var(--md-sys-color-tertiary)] text-[var(--md-sys-color-on-surface)] focus:text-[var(--md-sys-color-on-surface-variant)] transition-all duration-300'
+
+            />
+
+            {searchTerm && (
+                <button
+                    onClick={() => {
+                        setSearchTerm('');
+                        handleSearch({ target: { value: '' } });
+                    }}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[var(--md-sys-color-outline)]"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M18 6l-12 12" />
+                        <path d="M6 6l12 12" />
+                    </svg>
+                </button>
+            )}
         </div>
+
     );
 }
