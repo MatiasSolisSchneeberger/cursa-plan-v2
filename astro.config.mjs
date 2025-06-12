721470dedb017f058cca3442872b057ea6d2657e
MatiasSolisSchneeberger <matias.solis.sch@gmail.com>
2025-06-12 11:13:55 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwindcss from '@tailwindcss/vite';
 
