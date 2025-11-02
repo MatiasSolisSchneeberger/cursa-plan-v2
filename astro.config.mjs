6931064a085abb93e801be1864f32c6ee6f78ab5
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-11-02 20:57:05 -0300

    formatear la configuración de Astro para mejorar la legibilidad

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import vercel from '@astrojs/vercel';
- 
+
 import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
-  adapter: vercel(),
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    integrations: [react(), icon()],
+    adapter: vercel(),
+    output: 'server',
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
